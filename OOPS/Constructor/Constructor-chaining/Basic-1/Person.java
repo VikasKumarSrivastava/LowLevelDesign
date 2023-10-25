e54a21917dc21b7a7a52e95e9541b159359794a3
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-10-25 22:47:30 +0530

    Create Person.java

---
+++ OOPS/Constructor/Constructor-chaining/Basic-1/Person.java
@@ -0,0 +1,9 @@
+package practicelowlevel;
+
+public class Person {
+	int empID;
+	Person(int empID){
+		this.empID = empID;
+		System.out.println("Constructor from person and value is "+empID);
+	}
+}
