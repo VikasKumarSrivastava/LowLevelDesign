33e49e51a8cd04d6fd8e415f5ef9ef0f48cc77a0
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-10-25 22:47:11 +0530

    Create Main.java

---
+++ OOPS/Constructor/Constructor-chaining/Basic-1/Main.java
@@ -0,0 +1,11 @@
+package practicelowlevel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Manager mgr1 = new Manager("Vikas",101);
+
+	}
+
+}
