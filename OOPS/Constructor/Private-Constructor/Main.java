8c17ccdd417a178baf867e28d6f8a73a1616ee3e
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-10-25 23:18:15 +0530

    Create Main.java

---
+++ OOPS/Constructor/Private-Constructor/Main.java
@@ -0,0 +1,10 @@
+package practicelowlevel;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		TODO Auto-generated method stub
+		DBConnection db1 = DBConnection.getInstance();
+	}
+}
+
