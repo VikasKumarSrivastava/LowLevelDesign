3d2d2d104f42de37029d79199c29f01e1ce4e0ad
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-10-25 22:55:54 +0530

    Create Employee.java

---
+++ OOPS/Constructor/Constructor-chaining/Basic-1/Employee.java
@@ -0,0 +1,22 @@
+package practicelowlevel;
+
+public class Employee {
+	String name;
+	int empID;
+	//constructor 1
+	Employee(){
+		this(10);
+		System.out.println("No args constructor from Employee");
+	}
+	//constructor 2
+	Employee(int empID){
+		this(empID,"Vikas");
+		System.out.println("From constructor no 2");		
+	}
+	//constructor 3
+	Employee(int empID, String name){
+		this.empID = empID;
+		this.name = name;
+		System.out.println("From constructor no 3");		
+	}
+}
