dd8a9ff409a11719f240187a106b0ea4c15cd7b4
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-10-25 22:56:14 +0530

    Update Main.java

--- OOPS/Constructor/Constructor-chaining/Basic-1/Main.java
+++ OOPS/Constructor/Constructor-chaining/Basic-1/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Manager mgr1 = new Manager("Vikas",101);
+		Employee emp1 = new Employee();
 
 	}
 
