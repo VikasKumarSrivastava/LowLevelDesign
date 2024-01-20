75eb1b1a6b1ad3c7ea85bccd9b5e9423f27cf913
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-20 17:12:30 +0530

    Create NumberTerminalExpression.java

---
+++ DesignPattern/Interpreter/NumberTerminalExpression.java
@@ -0,0 +1,13 @@
+package BehavioralPattern_interpreter;
+
+public class NumberTerminalExpression implements AbstractExpression {
+	String str;
+	NumberTerminalExpression(String s){
+		str=s;
+	}
+	@Override
+	public int interpret(Context context) {
+		return context.get(str);
+	}
+
+}
